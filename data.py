e33d9ecb7f464f6567727db0de7c4abad053e6b3
ayushkanha <61038773+ayushkanha@users.noreply.github.com>
2025-04-14 18:33:04 +0530

    Update data.py

--- data.py
+++ data.py
@@ -35,6 +35,6 @@ docs = [Document(page_content=doc["content"], metadata={"title": doc["title"]}) 
 embedding_model = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
 
 # Createing FAISS vector store
-vector_store = Chroma.from_documents(docs, embedding_model)
+vector_store = docs
 
 
